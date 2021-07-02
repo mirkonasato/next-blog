193bf5acadb5f382c9c96ce9bdc7eb37849a76de
Mirko Nasato <mirko@nasato.dev>
2021-07-02 09:00:00 +0000

    Next Head Component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 function HomePage() {
   console.log('[HomePage] render');
   return (
     <>
+      <Head>
+        <title>My Blog</title>
+      </Head>
       <main>
         <h1>My Blog</h1>
       </main>
