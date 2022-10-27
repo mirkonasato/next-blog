1981b5922e400f0e903accc1cc2b8d91df3c9061
Mirko Nasato <mirko@nasato.dev>
2022-10-27 10:11:12 +0100

    Custom App Component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import NavBar from '../components/NavBar';
-
 function HomePage() {
   console.log('[HomePage] render');
   return (
     <>
-      <header>
-        <NavBar />
-      </header>
       <main>
         <h1>My Blog</h1>
       </main>
