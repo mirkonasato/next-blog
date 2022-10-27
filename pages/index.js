c254041c8aa926c18ec3d8f34f237f12722778d6
Mirko Nasato <mirko@nasato.dev>
2022-10-27 10:11:12 +0100

    Component Styles with Styled JSX

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,11 @@ function HomePage() {
       </Head>
       <main>
         <h1>My Blog</h1>
+        <ul>
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </ul>
       </main>
     </>
   );
