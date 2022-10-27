84dbb347fc6096714944992b05a68a9fcceb8551
Mirko Nasato <mirko@nasato.dev>
2022-10-27 10:11:12 +0100

    Saving Preferences to Local Storage

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,14 +1,24 @@
 import { useState } from 'react';
 import DarkTheme from './DarkTheme';
 
+function loadDarkMode() {
+  const value = localStorage.getItem('darkMode');
+  return (value === null) ? false : JSON.parse(value);
+}
+
 function ThemeSwitch() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(loadDarkMode);
+
+  const handleClick = () => {
+    localStorage.setItem('darkMode', JSON.stringify(!darkMode));
+    setDarkMode(!darkMode);
+  };
 
   console.log('[ThemeSwitch] darkMode:', darkMode);
   const text = darkMode ? 'Light Mode' : 'Dark Mode';
   return (
     <>
-      <button onClick={() => setDarkMode(!darkMode)}>
+      <button onClick={handleClick}>
         {text}
       </button>
       <style jsx>{`
