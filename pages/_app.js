0914c2bf8e8a6357e5e6324e0a2286aeb68b8ca2
Mirko Nasato <mirko@nasato.dev>
2022-10-27 10:11:12 +0100

    Images and Other Assets

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import NavBar from '../components/NavBar';
 import '../styles/globals.css';
 
 function App({ Component, pageProps }) {
   return (    
     <>
+      <Head>
+        <link rel="icon" href="/icons/favicon.ico" />
+      </Head>
       <header>
         <NavBar />
       </header>
