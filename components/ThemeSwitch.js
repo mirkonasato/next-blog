1d2ed74ac4d558f82401173787c896fd03e904c0
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    DarkTheme Component

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import DarkTheme from './DarkTheme';
 
 function ThemeSwitch() {
   const [darkMode, setDarkMode] = useState(false);
@@ -18,6 +19,7 @@ function ThemeSwitch() {
           cursor: pointer;
         }
       `}</style>
+      {darkMode && <DarkTheme />}
     </>
   );
 }
