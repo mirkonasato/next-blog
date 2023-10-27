a6f821452ebfe00a3079aebf90f335e38099ffa0
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Next Link Component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
+import Link from 'next/link';
+
 function HomePage() {
   console.log('[HomePage] render');
   return (
-    <main>
-      <h1>My Blog</h1>
-    </main>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h1>My Blog</h1>
+      </main>
+    </>
   );
 }
 
