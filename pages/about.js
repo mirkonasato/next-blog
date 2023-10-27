3991a9a774a2f122d3f2f4703e46d5d6f9e2cfae
Mirko Nasato <mirko@nasato.dev>
2023-10-27 17:44:29 +0100

    Next Head Component

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,12 @@
+import Head from 'next/head';
+
 function AboutPage() {
   console.log('[AboutPage] render');
   return (
     <>
+      <Head>
+        <title>About - My Blog</title>
+      </Head>
       <main>
         <h1>About</h1>
       </main>
